eaa6aa103a64dcce55b1b00a200846ddf54dcbf3
Renato Guimarães <renat.cg@gmail.com>
2026-09-12 10:29:34 -0300

    fix(rsvp): corrige datas dos dias do evento (09, 10 e 11/10)

--- server/rsvp.js
+++ server/rsvp.js
@@ -11,9 +11,9 @@ const GUESTS_FILE = path.join(DATA_DIR, 'guests.json');
 
 // Dias do evento. Mudar aqui reflete em toda a tela do convidado e no admin.
 const DAYS = {
-  dia1: { key: 'dia1', short: 'Dia 1', label: 'Sábado, 10 de outubro', weekday: 'Sábado', dateLabel: '10 de outubro de 2026', time: '15h' },
-  dia2: { key: 'dia2', short: 'Dia 2', label: 'Domingo, 11 de outubro', weekday: 'Domingo', dateLabel: '11 de outubro de 2026', time: '15h' },
-  dia3: { key: 'dia3', short: 'Dia 3', label: 'Segunda, 12 de outubro', weekday: 'Segunda-feira', dateLabel: '12 de outubro de 2026', time: '15h' },
+  dia1: { key: 'dia1', short: 'Dia 1', label: 'Sexta, 9 de outubro', weekday: 'Sexta-feira', dateLabel: '9 de outubro de 2026', time: '15h' },
+  dia2: { key: 'dia2', short: 'Dia 2', label: 'Sábado, 10 de outubro', weekday: 'Sábado', dateLabel: '10 de outubro de 2026', time: '15h' },
+  dia3: { key: 'dia3', short: 'Dia 3', label: 'Domingo, 11 de outubro', weekday: 'Domingo', dateLabel: '11 de outubro de 2026', time: '15h' },
 };
 
 function ensureFile() {
